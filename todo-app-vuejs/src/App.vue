50d672dbcda39578a2511a96bdb402f8315002fa
Trailerparkgirl <hulegee.w@hotmail.com>
2023-12-02 00:37:59 +0900

    nothing but functional todo

--- todo-app-vuejs/src/App.vue
+++ todo-app-vuejs/src/App.vue
@@ -1,47 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Todolist from './components/Todolist.vue';
 </script>
 
 <template>
-  <header>
+  <header class="header">
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Todolist/>
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px; /* Adjust as needed */
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  margin-bottom: 20px; /* Adjust as needed */
+  height: 75px;
 }
 </style>
---
+++ todo-app-vuejs/src/components/Todolist.vue
@@ -0,0 +1,145 @@
+<template>
+    <div>
+      <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+      <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" v-model="todo.completed">
+          <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
+          <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
+        </div>
+        <div class="remove-item" @click="removeTodo(index)">
+          &times;
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'todo-list',
+    data () {
+      return {
+        newTodo: '',
+        idForTodo: 3,
+        beforeEditCache: '',
+        todos: [
+          {
+            'id': 1,
+            'title': 'Finish Vue Screencast',
+            'completed': false,
+            'editing': false,
+          },
+          {
+            'id': 2,
+            'title': 'Take over world',
+            'completed': false,
+            'editing': false,
+          },
+        ]
+      }
+    },
+
+    directives: {
+      focus: {
+        inserted: function (el) {
+          el.focus()
+        }
+      }
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim().length == 0) {
+          return
+        }
+  
+        this.todos.push({
+          id: this.idForTodo,
+          title: this.newTodo,
+          completed: false,
+          editing: false,
+        })
+  
+        this.newTodo = ''
+        this.idForTodo++
+      },
+      editTodo(todo) {
+        this.beforeEditCache = todo.title
+        todo.editing = true
+      },
+      doneEdit(todo) {
+        if (todo.title.trim() == '') {
+          todo.title = this.beforeEditCache
+        }
+        todo.editing = false
+      },
+      cancelEdit(todo) {
+        todo.title = this.beforeEditCache
+        todo.editing = false
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1)
+      },
+    }
+  }
+  </script>
+  
+  <style lang="scss">
+    @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+  
+    .todo-input {
+      width: 100%;
+      padding: 10px 18px;
+      font-size: 18px;
+      margin-bottom: 16px;
+  
+      &:focus {
+        outline: 0;
+      }
+    }
+  
+    .todo-item {
+      margin-bottom: 12px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      animation-duration: 0.3s;
+    }
+  
+    .remove-item {
+      cursor: pointer;
+      margin-left: 14px;
+      &:hover {
+        color: rgb(0, 0, 0);
+      }
+    }
+  
+    .todo-item-left { // later
+      display: flex;
+      align-items: center;
+    }
+  
+    .todo-item-label {
+      padding: 4px;
+      border: 2px solid white;
+      margin-left: 12px;
+    }
+  
+    .todo-item-edit {
+      font-size: 24px;
+      color: #2c3e50;
+      margin-left: 12px;
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc; //override defaults
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  
+      &:focus {
+        outline: none;
+      }
+    }
+  
+    .completed {
+      text-decoration: line-through;
+      color: grey;
+    }
+  </style>
